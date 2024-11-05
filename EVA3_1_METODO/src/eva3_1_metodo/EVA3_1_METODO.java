ae12b58c1ad7094d4bbb3d0f57fd08cebc1d4bda
Camlimas <l24550242@chihuahua2.tecnm.mx>
2024-11-05 10:01:15 -0600

    Add files via upload

---
+++ EVA3_1_METODO/src/eva3_1_metodo/EVA3_1_METODO.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva3_1_metodo;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA3_1_METODO {
+    public static void main(String[] args) {
+        //USO DE LOS METODOS:
+        //INVOCACIÓN O LLAMADA
+        imprimirMensaje("Hola mundo!!");
+    }
+    
+    
+    //DECLARACIO
+    //header (encabezado)
+    //IMPRIMIR UN MENSAJE
+    // JAVA:iniciar en minúsculas,verbo,siguiente palabra Inicia con mayúsculas
+    public static void imprimirMensaje(String mensaje){
+      System.out.println("TU MENSAJE:" + mensaje);
+    } 
+            
+}
